5ebb0899b701209519ba75dfeaf9643111eef223
Ben Ford <ben.ford@puppetlabs.com>
2015-02-05 05:36:02 -0800

    support uninstallation

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+﻿$wrapperExe  = 'C:\ProgramData\chocolatey\bin\nssm.exe'
+
+try {
+  Invoke-Expression "$wrapperExe stop nginx"
+  Invoke-Expression "$wrapperExe remove nginx"
+
+  Write-ChocolateySuccess "nginx-service"
+} catch {
+  Write-ChocolateyFailure "nginx-service" "$($_.Exception.Message)"
+  throw
+}
